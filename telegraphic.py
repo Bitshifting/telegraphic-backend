5c896b9e9e1200c1d4691149dce0513337497e8a
Alex Kersten <alex@dividebyxero.com>
2014-09-06 05:01:02 -0500

    finished user registration function

--- telegraphic.py
+++ telegraphic.py
@@ -12,6 +12,55 @@ def log(msg):
     print('[' + request.remote_addr + '] ' + msg)
 
 
+def fail(msg):
+    """Returns a failure JSON to the user."""
+    return {'success': False, 'message': msg}
+
+
+def success(msg):
+    """Returns a success JSON to the user."""
+    return {'success': True, 'message': msg}
+
+
+# ######################################################################################################################
+# User Accounts
+# ######################################################################################################################
+
+# Register user
+
+@post('/user/register')
+def registerUser():
+    if not 'username' in request.json.keys():
+        return fail('Username not provided in registration.')
+    if not 'passwordHash' in request.json.keys():
+        return fail('Password hash not provided in registration.')
+
+    log("Registering new user account: " + request.json['username'] + " " + request.json['passwordHash'])
+
+    # Check that this user doesn't already exist.
+    con = database.connect()
+    c = con.cursor()
+    c.execute("SELECT COUNT(*) FROM users WHERE username=:username", {'username': request.json['username']})
+
+    r = c.fetchone()
+    if r[0] != 0:
+        log("That username already exists.")
+        return fail('Username already exists.')
+
+    # Ok. Insert them into the database.
+    c.execute("INSERT INTO users (username, passwordHash) VALUES (:username, :passwordHash)",
+              {'username': request.json['username'], 'passwordHash': request.json['passwordHash']})
+
+    database.close(con)
+
+    log("User registered.")
+    return success('User registered.')
+
+# ######################################################################################################################
+# Image Handling
+# ######################################################################################################################
+
+
 
 print("Creating tables if need be...")
 database.createTables()
