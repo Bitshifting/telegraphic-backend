6e6662b506b4b883566cceb6cf526ecfc3116a11
Alex Kersten <alex@dividebyxero.com>
2014-09-06 04:42:46 -0500

    add table creation queries

---
+++ queries.py
@@ -0,0 +1,48 @@
+# ######################################################################################################################
+# Tables
+# ######################################################################################################################
+
+CREATE_TABLE_USERS = """
+    CREATE TABLE IF NOT EXISTS users (
+         userID INTEGER PRIMARY KEY AUTOINCREMENT,
+         username TEXT NOT NULL,
+         passwordHash TEXT NOT NULL,
+         createdOn INTEGER DEFAULT strftime('%s', 'now') NOT NULL
+     )
+"""
+
+CREATE_TABLE_ACTIVE_ACCESS_TOKENS = """
+    CREATE TABLE IF NOT EXISTS activeAccessTokens (
+        tokenID INTEGER PRIMARY KEY AUTOINCREMENT,
+        accessToken TEXT NOT NULL,
+        username REFERENCES users (username),
+        createdOn INTEGER DEFAULT strftime('%s', 'now') NOT NULL
+    )
+"""
+
+CREATE_TABLE_IMAGES = """
+    CREATE TABLE IF NOT EXISTS images (
+        imageID INTEGER PRIMARY KEY AUTOINCREMENT,
+        originalOwner REFERENCES users (username),
+        createdOn INTEGER DEFAULT strftime('%s', 'now') NOT NULL,
+        hopsLeft INTEGER NOT NULL,
+        editTime INTEGER NOT NULL,
+        image BLOB NOT NULL
+    )
+"""
+
+# Use to keep a one-to-many relationship between images and their previous owners to determine who to show the completed
+# image to once it runs out of hops.
+CREATE_TABLE_IMAGE_HISTORY = """
+    CREATE TABLE IF NOT EXISTS imageHistory (
+        historyID INTEGER PRIMARY KEY AUTOINCREMENT,
+        imageID REFERENCES images (imageID),
+        username REFERENCES users (username)
+    )
+"""
+
+
+# ######################################################################################################################
+# Queries
+# ######################################################################################################################
+
