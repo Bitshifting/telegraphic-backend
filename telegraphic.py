4c4d97e15854b77b2558b729cfd1f345fb533b09
Alex Kersten <alex@dividebyxero.com>
2014-09-06 05:14:03 -0500

    finished user login function

--- telegraphic.py
+++ telegraphic.py
@@ -1,5 +1,6 @@
 from bottle import error, get, post, run, request
 import database
+import uuid
 
 
 @error(404)
@@ -29,10 +30,12 @@ def success(msg):
 # Register user
 
 @post('/user/register')
-def registerUser():
+def userRegister():
     if not 'username' in request.json.keys():
+        log('Registration attempt but no username provided in request.')
         return fail('Username not provided in registration.')
     if not 'passwordHash' in request.json.keys():
+        log('Registration attempt but no password hash provided in request.')
         return fail('Password hash not provided in registration.')
 
     log("Registering new user account: " + request.json['username'] + " " + request.json['passwordHash'])
@@ -55,6 +58,37 @@ def registerUser():
 
     log("User registered.")
     return success('User registered.')
+
+
+@post('/user/login')
+def userLogin():
+    if not 'username' in request.json.keys():
+        log('Login attempt but no username provided in request.')
+        return fail('Username not provided in login.')
+    if not 'passwordHash' in request.json.keys():
+        log('Login attempt but no password hash provided in request.')
+        return fail('Password hash not provided in login.')
+
+    log('Attempt login ' + request.json['username'] + ' ' + request.json['passwordHash'])
+
+    # Check if this is the correct password.
+    con = database.connect()
+    c = con.cursor()
+    c.execute("SELECT COUNT(*) FROM users WHERE username=:username AND passwordHash=:passwordHash",
+              {'username': request.json['username'], 'passwordHash': request.json['passwordHash']})
+
+    r = c.fetchone()
+    if r[0] == 0:
+        log('Login failed.')
+
+    # Alright, they've logged in, now generate a random access key and give it to them.
+    # XXX: These probably won't collide.
+    accessKey = str(uuid.uuid1())
+
+    c.execute("INSERT INTO activeAccessTokens (accessToken, username) VALUES (:accessKey, :username)",
+              {'accessToken': accessKey, 'username': request.json['username']})
+
+    return {'success': True, 'accessToken': accessKey, 'message': 'Logged in.'}
 
 # ######################################################################################################################
 # Image Handling
